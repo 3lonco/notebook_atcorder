e25b38959e1d29c563d4c3e64c6572e619a39bde
JunKubota <kinkado115@gmail.com>
2022-10-26 20:17:13 +0900

    aaaa xxxxxx

---
+++ test.py
@@ -0,0 +1,18 @@
+P,Q,R=map(int,input().split())
+a=[]
+a.append(P)
+a.append(Q)
+a.append(R)
+
+
+import itertools
+
+ans=100000000000000
+nums = [0, 1, 2]
+for balls in itertools.permutations(nums, 2):
+    tmp=0
+    for s in balls:
+        tmp+=a[s]
+    ans=min(ans,tmp)
+
+print(ans)
